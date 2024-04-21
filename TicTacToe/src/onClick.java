049d35098e2c9f8e79503cd607a1be93014a2eee
Abdullah-hmed <abdullahwyi@gmail.com>
2024-04-21 23:26:30 +0500

    Removing Unused Imports

--- TicTacToe/src/onClick.java
+++ TicTacToe/src/onClick.java
@@ -1,12 +1,8 @@
 
-import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
